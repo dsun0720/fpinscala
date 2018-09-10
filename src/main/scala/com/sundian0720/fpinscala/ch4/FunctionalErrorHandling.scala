24b2814a681573197adb69741a98e8158c2025b1
Dian SUN <sun.dian0720@gmail.com>
2018-09-10 17:13:48 +0200

    fpinscala-40: either

--- src/main/scala/com/sundian0720/fpinscala/ch4/FunctionalErrorHandling.scala
+++ src/main/scala/com/sundian0720/fpinscala/ch4/FunctionalErrorHandling.scala
@@ -61,5 +61,47 @@ object FunctionalErrorHandling {
     sequence(a.map(f))
   }
 
+  sealed trait Either[+E, +A] {
+
+    def map[B](f: A => B): Either[E, B] = this match {
+      case Left(v) => Left(v)
+      case Right(v) => Right(f(v))
+    }
+
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+      case Left(v) => Left(v)
+      case Right(v) => f(v)
+    }
+
+    def orElse[EE >: E, B >: A](f: A => Either[EE, B]): Either[EE, B] = this match {
+      case Left(v) => f(v)
+      case _: Right[B] => this
+    }
+
+    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+      for (aa <- this;
+           bb <- b
+      )
+        yield f(aa, bb)
+    }
+
+
+  }
+
+  case class Left[+E](value: E) extends Either[E, Nothing]
+
+  case class Right[+A](value: A) extends Either[Nothing, A]
+
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = es.foldLeft(Right(List.empty[A]): Either[E, List[A]]) {
+    case (_, left: Left[E]) => left
+    case (left: Left[E], _) => left
+    case (Right(list), Right(v)) => Right(v :: list)
+  }
+
+  def traverse[E, A, B](as: List[A])(
+    f: A => Either[E, B]): Either[E, List[B]] = {
+    sequence(as.map(f))
+  }
+
 
 }
